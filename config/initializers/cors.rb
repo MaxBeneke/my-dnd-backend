f758e3e914fe4e422ef26349af750cf062c386a0
Max Beneke <max.beneke@gmail.com>
2021-04-27 15:41:06 -0400

    add http

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'my-dnd.netlify.app'
+    origins 'https://my-dnd.netlify.app'
 
     resource '*',
       headers: :any,
